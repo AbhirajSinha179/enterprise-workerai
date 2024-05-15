f02f3b9012e3ce43c24c6134b600d1d059d79235
ayanjamil <ayanjamil00@gmail.com>
2024-05-16 00:47:16 +0530

    working multiselect with badge

--- app/(main)/dashboard/outboundSetting/page.tsx
+++ app/(main)/dashboard/outboundSetting/page.tsx
@@ -1,4 +1,7 @@
+"use client"
 import CardCustom from "@/components/customComponents/customCard"
+import SelectDemo from "@/components/customComponents/multipleSelectChip"
+
 import { ContentLayout } from "@/components/layout/content-layout"
 
 export default async function outboundSetting() {
@@ -6,7 +9,16 @@ export default async function outboundSetting() {
     <>
       <ContentLayout title="Outbound Settings">
         <div className="container mx-auto my-10 flex justify-between">
-          <CardCustom title="Users" />
+          <div className="m-4 h-96 w-1/2 overflow-hidden rounded-lg bg-zinc-600 shadow-md transition duration-300 hover:shadow-lg">
+            <div className="p-4">
+              <h2 className="mb-2 text-xl font-semibold text-zinc-50">Users</h2>
+            </div>
+            <div className=" flex justify-center ">
+              <div className="m-4 w-5/6 rounded-md bg-background  ">
+                <SelectDemo />
+              </div>
+            </div>
+          </div>
           <CardCustom title="Company data" />
         </div>
         <div className="container mx-auto my-10 flex justify-between  ">
---
+++ components/customComponents/multipleSelectChip.tsx
@@ -0,0 +1,65 @@
+import { X } from "lucide-react"
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Badge } from "../ui/badge"
+
+const OPTIONS = [
+  "Next.js",
+  "SvelteKit",
+  "Nuxt.js",
+  "tree",
+  "track",
+  "traffic",
+  "track",
+  "recket",
+  "treat",
+  "tampon",
+  "Truck",
+  "suck",
+  "Chuck",
+]
+
+export default function SelectDemo() {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([])
+
+  const handleSelect = (value: string) => {
+    if (!selectedOptions.includes(value)) {
+      setSelectedOptions((prev) => [...prev, value])
+    }
+  }
+
+  const handleRemove = (value: string) => {
+    setSelectedOptions((prev) => prev.filter((option) => option !== value))
+  }
+
+  return (
+    <div className="min-w-8">
+      <Select onValueChange={handleSelect}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select an option" />
+        </SelectTrigger>
+        <SelectContent>
+          {OPTIONS.map((option) => (
+            <SelectItem key={option} value={option}>
+              {option}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <div className="no-scrollbar flex max-h-16 flex-wrap gap-2 overflow-y-auto rounded-md bg-primary-foreground p-2">
+        {selectedOptions.map((option) => (
+          <button key={option} onClick={() => handleRemove(option)}>
+            <Badge variant="secondary">
+              <div className="flex items-center justify-around gap-1">
+                <div>{option}</div>
+                {/* icon chota crow, x ko clickable banao, but in a circular button (define button ka width and height) */}
+                <X className="h-4 w-4" />
+              </div>
+            </Badge>
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
