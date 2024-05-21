51f9e5a48ed8c924d603321502ca4b95ab052576
ayanjamil <ayan.jamil@fluidanalytics.ai>
2024-05-21 22:09:27 +0530

    working Leads page

--- app/(main)/dashboard/leads/page.tsx
+++ app/(main)/dashboard/leads/page.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from "next"
+import Image from "next/image"
 import Link from "next/link"
+import { z } from "zod"
+import { promises as fs } from "fs"
+import path from "path"
 import { ContentLayout } from "@/components/layout/content-layout"
+import { columns } from "@/components/leads/columns"
+import { DataTable } from "@/components/leads/data-table"
+import { UserNav } from "@/components/leads/user-nav"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,8 +16,26 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
+import { taskSchema } from "@/data/schema"
 
-export default function Leads() {
+
+async function getTasks() {
+  const data = await fs.readFile(
+    // path.join(process.cwd(), "app/(app)/examples/tasks/data/tasks.json")
+    path.join(process.cwd(), "/data/tasks.json")
+  )
+
+  // use fetch to get data with pagination
+  // use response, check Nextjs docs for fetching and validating data
+  // also understand what server actions, dynamic fetching is like
+
+  const tasks = JSON.parse(data.toString())
+
+  return z.array(taskSchema).parse(tasks)
+}
+
+export default async function Leads() {
+  const tasks = await getTasks()
   return (
     <ContentLayout title="Leads">
       <Breadcrumb>
@@ -26,6 +52,36 @@ export default function Leads() {
         </BreadcrumbList>
       </Breadcrumb>
       <main className="flex items-center justify-center">Random leads</main>
+      <div className="md:hidden">
+        <Image
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
     </ContentLayout>
   )
 }
