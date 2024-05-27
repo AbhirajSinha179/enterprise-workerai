cb5f7c6da040fe530bcb00f138d3a538745481dc
ayanjamil <ayan.jamil@fluidanalytics.ai>
2024-05-27 12:00:36 +0530

    resolving heroPost issue

--- app/(site)/(blog)/blog/page.tsx
+++ app/(site)/(blog)/blog/page.tsx
@@ -4,24 +4,22 @@ import { MoreStories } from "@/components/blogs/more-stories";
 import { getAllPosts } from "@/lib/api";
 
 const Blogs = async () => {
-  const allPosts = getAllPosts();
+  const allPosts = await getAllPosts();
   const heroPost = allPosts[0];
   const morePosts = allPosts.slice(1);
 
   return (
     <>
-      {/* <div>
-        <h1 className="pt-10 text-center text-4xl font-bold">Worker AI Blog</h1>
-      </div> */}
-      {/* <hr className="mt-10 border-t border-gray-200" /> */}
       <div className="mt-20">
         <div className="container mx-auto px-5">
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.coverImage}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
-          />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </div>
       </div>
