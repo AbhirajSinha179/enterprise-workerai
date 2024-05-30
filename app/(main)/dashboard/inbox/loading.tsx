d1fa96c713c673213675b663e2f4586018fc005f
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2024-05-31 02:13:03 +0530

    feat: add loading.tsx to inbox and scheduler components initialising

--- app/(main)/dashboard/inbox/loading.tsx
+++ app/(main)/dashboard/inbox/loading.tsx
@@ -1,7 +1,7 @@
+import LoadingSign from "@/components/global/loading"
+
 export default function Loading() {
   return (
-    <>
-      <p className="text-2xl font-bold">Loading...</p>
-    </>
+    <LoadingSign />
   )
 }
---
+++ components/scheduler/scheduled-emails.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils";
+import { formatDistanceToNow } from "date-fns";
+
+export function ScheduledEmailList({ emails }: any) {
+  return (
+    <main className="flex w-full flex-col items-center">
+      <div className="mt-6 flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Scheduled Emails</h1>
+      </div>
+      <ul className="my-4 w-full space-y-4">
+        {emails.map((item: any) => (
+          <div
+            key={item.id}
+            className={cn("flex w-full flex-col items-start gap-2 rounded-lg border p-4 text-left transition-all")}
+          >
+            <div className="flex w-full flex-col gap-1">
+              <div className="flex items-center">
+                <div className="text-2xl font-semibold">{item.id}</div>
+                <div className={cn("ml-auto")}>
+                  {formatDistanceToNow(new Date(item.date), {
+                    addSuffix: true,
+                  })}
+                </div>
+              </div>
+              <div className="text-md font-medium">{item.subject}</div>
+            </div>
+            <div className="text-xs text-muted-foreground">{item.body}</div>
+          </div>
+        ))}
+      </ul>
+    </main>
+  )
+}
