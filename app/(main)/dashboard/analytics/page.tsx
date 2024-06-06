7ef6b72d6430e630325f023730e0d4deb051b7f2
ayanjamil <ayan.jamil@fluidanalytics.ai>
2024-06-06 19:33:49 +0530

    resolving comments of analytics page

--- app/(main)/dashboard/analytics/page.tsx
+++ app/(main)/dashboard/analytics/page.tsx
@@ -1,3 +1,4 @@
+import { Eye, MailOpen, MessageSquare, Send, Target } from "lucide-react"
 import { Overview } from "@/components/dashboard/overview"
 import { RecentSales } from "@/components/dashboard/recent-sales"
 import { ContentLayout } from "@/components/layout/content-layout"
@@ -10,15 +11,12 @@ export default function AnalyticsPage() {
         <div className="grid gap-x-4 md:grid-cols-2 lg:grid-cols-4">
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Open Rate</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="text-4xl font-bold">62%</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Total Sent</CardTitle>
+              <Send
+                className="size-4 text-muted-foreground"
+                width={24}
+                height={24}
+              />
 
             </CardHeader>
             <CardContent>
@@ -27,7 +25,31 @@ export default function AnalyticsPage() {
           </Card>
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Open Rate</CardTitle>
+              <MailOpen
+                className="size-4 text-muted-foreground"
+                width={24}
+                height={24} />
+            </CardHeader>
+
+            <CardContent>
+              <div className="text-4xl font-bold">62%</div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Response Rate</CardTitle>
+              {/* <MessageSquare
+                className="size-4 text-muted-foreground"
+                width={24}
+                height={24}
+              /> */}
+              <Eye
+                className="size-4 text-muted-foreground"
+                width={24}
+                height={24}
+              />
             </CardHeader>
             <CardContent>
               <div className="text-4xl font-bold">5%</div>
@@ -36,6 +58,11 @@ export default function AnalyticsPage() {
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Monthly target</CardTitle>
+              <Target
+                className="size-4 text-muted-foreground"
+                width={24}
+                height={24}
+              />
             </CardHeader>
             <CardContent>
               <div className="text-4xl font-bold">1249/1500</div>
