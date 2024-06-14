d51e3c65718591d38aa3c32c980b423203e46d0d
8rxn <rajaryanvishwakarma10@gmail.com>
2024-06-14 19:13:09 +0530

    fix: force dark mode on site
    Signed-off-by: 8rxn <rajaryanvishwakarma10@gmail.com>

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,14 +1,17 @@
 import Footer from "@/components/global/footer";
 import { SiteNav } from "@/components/navbar/site-nav";
+import { DarkModeWrapper } from "@/providers/force-dark-provider";
 
 export default function Layout({ children }: {
   children: React.ReactNode;
 }) {
   return (
     <>
+    <DarkModeWrapper>
       <SiteNav />
       {children}
       <Footer />
+    </DarkModeWrapper>
     </>
   );
 }
