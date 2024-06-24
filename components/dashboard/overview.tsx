476f7b8cc4591d31b417d27ef4578493028675da
ayanjamil <ayan.jamil@fluidanalytics.ai>
2024-06-24 20:56:01 +0530

    fix:light mode dashboard graph

--- components/dashboard/overview.tsx
+++ components/dashboard/overview.tsx
@@ -58,12 +58,12 @@ export function Overview() {
       >
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#F4EEE0" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#F4EEE0" stopOpacity={0} />
+            <stop offset="5%" stopColor="#808080" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#808080" stopOpacity={0} />
           </linearGradient>
           <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#F4EEE0" stopOpacity={1.0} />
-            <stop offset="95%" stopColor="#F4EEE0" stopOpacity={0} />
+            <stop offset="5%" stopColor="#808080" stopOpacity={1.0} />
+            <stop offset="95%" stopColor="#808080" stopOpacity={0} />
           </linearGradient>
         </defs>
         <XAxis dataKey="name" />
@@ -79,7 +79,7 @@ export function Overview() {
         <Area
           type="monotone"
           dataKey="pv"
-          stroke="#F4EEE0"
+          stroke="#808080"
           fillOpacity={1}
           fill="url(#colorPv)"
         />
