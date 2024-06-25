ef5f88aabedb5fb1822472429e2be62cb39e0a57
ayanjamil <ayan.jamil@fluidanalytics.ai>
2024-06-25 19:43:21 +0530

    fix: separate interface file for mails

--- app/(main)/dashboard/inbox/page.tsx
+++ app/(main)/dashboard/inbox/page.tsx
@@ -1,53 +1,14 @@
-// import Link from "next/link"
-// import { threads } from "@/components/inbox/data"
-import { Inbox } from "@/components/inbox/mail"
-import { ContentLayout } from "@/components/layout/content-layout"
-// import {
-//   Breadcrumb,
-//   BreadcrumbItem,
-//   BreadcrumbLink,
-//   BreadcrumbList,
-//   BreadcrumbPage,
-//   BreadcrumbSeparator,
-// } from "@/components/ui/breadcrumb"
-
-type Email = {
-  id: string;
-  name: string;
-  email: string;
-  subject: string;
-  text: string;
-  date: string;
-  read: boolean;
-  labels: string[];
-};
-
-type Thread = {
-  threadid: string;
-  thread: Email[];
-};
-
-
-
-async function getData() {
-  const res = await fetch('http://localhost:3000/threads')
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  const data = await res.json();
-  return data as Thread[]
-}
+// app/(main)/dashboard/inbox/page.tsx
+import { Inbox } from "@/components/inbox/mail";
+import { ContentLayout } from "@/components/layout/content-layout";
+import { getData } from "@/lib/data-fetching";
 
 export default async function InboxPage() {
-  const threadData: Thread[] = await getData();
-  // console.log("resposne sssss: ", data)
-  // console.log("threadssss: ", threads)
+  const threadData = await getData();
 
   return (
-    <>
-      <ContentLayout title="Inbox">
-        <Inbox threads={threadData} />
-      </ContentLayout>
-    </>
-  )
+    <ContentLayout title="Inbox">
+      <Inbox threads={threadData} />
+    </ContentLayout>
+  );
 }
---
+++ lib/data-fetching.ts
@@ -0,0 +1,19 @@
+// app/lib/data-fetching.ts
+import { threadsSchema } from "./schema"
+
+async function getData() {
+  const res = await fetch("http://localhost:3000/threads")
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+  const data = await res.json()
+
+  const result = threadsSchema.safeParse(data)
+  if (!result.success) {
+    console.error(result.error)
+    throw new Error("Invalid data format")
+  }
+  return result.data
+}
+
+export { getData }
