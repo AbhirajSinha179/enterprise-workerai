aebe5bd45244272c4e8b94a97bc8e0bcc2711380
ayanjamil <ayan.jamil@fluidanalytics.ai>
2024-06-26 18:29:09 +0530

    feat: Server function for inbox data fetch

--- app/(main)/dashboard/inbox/page.tsx
+++ app/(main)/dashboard/inbox/page.tsx
@@ -1,7 +1,22 @@
-// app/(main)/dashboard/inbox/page.tsx
 import { Inbox } from "@/components/inbox/mail";
 import { ContentLayout } from "@/components/layout/content-layout";
-import { getData } from "@/lib/data-fetching";
+import { threadsSchema } from "@/types/interface"
+
+async function getData() {
+  const res = await fetch("http://localhost:3000/threads")
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+  const data = await res.json()
+  const result = threadsSchema.safeParse(data)
+  if (!result.success) {
+    console.error(result.error)
+    throw new Error("Invalid data format")
+  }
+  return result.data
+}
+
+export { getData }
 
 export default async function InboxPage() {
   const threadData = await getData();
