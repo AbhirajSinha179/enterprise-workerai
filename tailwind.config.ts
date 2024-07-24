44a30eae624a642e7f0c7fcce277d91147f8ad03
ayanjamil <ayanjamil00@gmail.com>
2024-07-24 20:19:59 +0530

    Revert "feat: rich text editor rough"
    This reverts commit 05ef8fa1f4d0790eec670e24c1ff3cc6be7c3bd3.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette")
 
-const config: Config = {
+const config = {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
@@ -82,7 +82,7 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate"), addVariablesForColors],
-}
+} satisfies Config
 
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"))
