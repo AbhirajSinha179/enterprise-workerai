7f6333955a866f7ef4927af7298f2b5e40c35f77
ayanjamil <ayanjamil00@gmail.com>
2024-08-17 20:31:32 +0530

    feat: pagination in unschedule emails

--- app/(main)/dashboard/emails/page.tsx
+++ app/(main)/dashboard/emails/page.tsx
@@ -7,73 +7,6 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { UnscheduledEmailThread, unscheduledEmailResponseSchema, scheduledEmailResponseSchema, ScheduledEmail } from "@/types/interface";
 
 
-export async function fetchUnscheduledEmails(targetId: string): Promise<UnscheduledEmailThread[]> {
-  const url = `${process.env.API_BASE_URL}/emails/unscheduled/${targetId}`;
-  // console.log("URL IS : ", url);
-
-  try {
-    const res = await fetch(url);
-    console.log(`Response status for get unscheduled email: ${res.status}`);
-
-    if (!res.ok) {
-      throw new Error(`Failed to fetch data. Status code: ${res.status}`);
-    }
-
-    const data = await res.json();
-    // console.log("DATA : ", data);
-    const emails = transformData(data);
-    // console.log("TRANSFORMED DATA : ", emails);
-    const result = unscheduledEmailResponseSchema.safeParse(emails);
-    // console.log("PARSE RESULT : ", result);
-
-    if (!result.success) {
-      console.error(result.error);
-      throw new Error("Invalid data format");
-    }
-
-    // console.log("THIS IS THE RESPONSE FROM SCHEDULER : ", result.data);
-    return result.data;
-  } catch (error: unknown) {
-    handleFetchError(error);
-    throw error;
-  }
-}
-
-
-function transformData(data: any): UnscheduledEmailThread[] {
-  return data.map((item: any) => ({
-    id: item.email.id,
-    threadId: item.email.threadId,
-    subject: item.email.subject,
-    body: item.email.body,
-    sender: item.email.recipient,
-    createdAt: item.email.createdAt,
-    updatedAt: item.email.updatedAt ?? null, // or other appropriate field if there's no `updatedAt`
-    status: item.lead.EmailAddStatus, // Assuming this is a status field
-    opened: item.email.opened,
-    clicked: item.email.clicked,
-    replied: item.email.replied,
-    bounced: item.email.bounced,
-    leadInfo: {
-      id: item.lead.id,
-      email: item.lead.email,
-      firstName: item.lead.firstName,
-      lastName: item.lead.lastName,
-      seniority: item.lead.seniority,
-    },
-    senderId: item.senderId
-  }));
-}
-
-
-function handleFetchError(error: unknown) {
-  if (error instanceof Error) {
-    console.error("Error fetching data:", error.message);
-  } else {
-    console.error("Unknown error fetching data:", error);
-  }
-}
-
 export async function fetchScheduledEmails(targetId: string): Promise<ScheduledEmail[]> {
   try {
     const url = `${process.env.API_BASE_URL}/emails/scheduled/${targetId}`;
@@ -85,7 +18,7 @@ export async function fetchScheduledEmails(targetId: string): Promise<ScheduledE
 
     const data = await res.json();
     const result = scheduledEmailResponseSchema.safeParse(data);
-    console.log("RESULT FROM SCHEDULED : ", result);
+    // console.log("RESULT FROM SCHEDULED : ", result);
     if (!result.success) {
       console.error(result.error);
       throw new Error("Invalid data format");
@@ -99,14 +32,8 @@ export async function fetchScheduledEmails(targetId: string): Promise<ScheduledE
 }
 
 export default async function Emails() {
-  const unscheduledEmails = await fetchUnscheduledEmails("1c1108a8-9108-42e2-8177-4e655bbc87ed");
   const scheduledEmails = await fetchScheduledEmails("1c1108a8-9108-42e2-8177-4e655bbc87ed");
-  console.log("Response is from get scheduled email API", scheduledEmails);
-
-  if (!unscheduledEmails || !scheduledEmails) {
-    return null;
-  }
-
+  // console.log("Response is from get scheduled email API", scheduledEmails);
   return (
     <ContentLayout title="Scheduler">
       <Tabs defaultValue="pending" className="mt-4 space-y-4">
@@ -116,20 +43,7 @@ export default async function Emails() {
         </TabsList>
 
         <TabsContent value="pending" className="space-y-4">
-          {(!unscheduledEmails || unscheduledEmails.length === 0) ? (
-            <>
-              <div className="mt-6 flex w-full items-center justify-between">
-                <h1 className="text-2xl font-bold">Pending Emails</h1>
-              </div>
-              <EmptyState
-                headerMessage="No pending Emails"
-                containerMessage="Go to Scheduled to check currently scheduled emails"
-                icon={<ClockIcon size={80} />}
-              />
-            </>
-          ) : (
-            <EmailList emails={unscheduledEmails} />
-          )}
+          <EmailList targetId="1c1108a8-9108-42e2-8177-4e655bbc87ed" />
         </TabsContent>
 
         <TabsContent value="scheduled" className="space-y-4">
--- components/scheduler/email-list.tsx
+++ components/scheduler/email-list.tsx
@@ -1,51 +1,115 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { format } from "date-fns";
 import { toast } from "sonner";
 import EmailItem from "./email-item";
 import { Button } from "../ui/button";
 import { UnscheduledEmailThread } from "@/types/interface";
-import { Label } from "@/components/ui/label"
-import { Switch } from "@/components/ui/switch"
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { useState } from "react";
+} from "@/components/ui/tooltip";
+import { PaginationWithLinks } from "../ui/pagination-with-links";
+import EmptyState from "@/components/global/empty-state";
+import { ClockIcon } from "lucide-react";
+import { unscheduledEmailResponseSchema } from "@/types/interface";
+import Loading from "@/app/(site)/loading";
 
-//pagination,on hower dialog, edit adn then approve all , switch default should be true
 interface EmailListProps {
-  emails: UnscheduledEmailThread[];
+  targetId: string;
 }
+
 function getUserId() {
-  //get user ID 
-  const userID = "user_2jQ7lufOqU1WFrEsi2wG3B7zF70"
-  return userID
+  return "user_2jQ7lufOqU1WFrEsi2wG3B7zF70";
 }
 
-function deleteEmail(emails: UnscheduledEmailThread[], emailId: string) {
-  emails = emails.filter((email) => email.id !== emailId);
-  //remove the mail from UI 
+function transformData(data: any): UnscheduledEmailThread[] {
+  return data.map((item: any) => ({
+    id: item.email.id,
+    threadId: item.email.threadId,
+    subject: item.email.subject,
+    body: item.email.body,
+    sender: item.email.recipient,
+    createdAt: item.email.createdAt,
+    updatedAt: item.email.updatedAt ?? null,
+    status: item.lead.EmailAddStatus,
+    opened: item.email.opened,
+    clicked: item.email.clicked,
+    replied: item.email.replied,
+    bounced: item.email.bounced,
+    leadInfo: {
+      id: item.lead.id,
+      email: item.lead.email,
+      firstName: item.lead.firstName,
+      lastName: item.lead.lastName,
+      seniority: item.lead.seniority,
+    },
+    senderId: item.senderId,
+  }));
 }
 
-export function EmailList({ emails }: EmailListProps) {
-  const [fetchedEmails, setFetchedEmails] = useState<UnscheduledEmailThread[]>(emails);
+export function EmailList({ targetId }: EmailListProps) {
+  const [fetchedEmails, setFetchedEmails] = useState<UnscheduledEmailThread[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
+
+  const fetchUnscheduledEmails = async (targetId: string, limit: number, skip: number): Promise<UnscheduledEmailThread[]> => {
+    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/emails/unscheduled/${targetId}?limit=${limit}&skip=${skip}`;
+    console.log(url)
+
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error(`Failed to fetch data. Status code: ${res.status}`);
+      }
+
+      const data = await res.json();
+      const emails = transformData(data);
+      const result = unscheduledEmailResponseSchema.safeParse(emails);
+
+      if (!result.success) {
+        throw new Error("Invalid data format");
+      }
+
+      return result.data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  };
+
+  const handleFetchEmails = async (page: number) => {
+    const skip = (page - 1) * pageSize;
+    setLoading(true);
+    setError(null);
+    try {
+      const emails = await fetchUnscheduledEmails(targetId, pageSize, skip);
+      setFetchedEmails(emails);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    handleFetchEmails(currentPage);
+  }, [currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   const handleApprove = async ({ emailId }: { emailId: string }) => {
     try {
       const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/schedule`;
-      console.log("URL: ", url);
       const payload = { emailId: emailId };
       const response = await fetch(url, {
         method: "POST",
@@ -58,11 +122,12 @@ export function EmailList({ emails }: EmailListProps) {
       if (!response.ok) {
         throw new Error(`Failed to approve draft. Status code: ${response.status}`);
       }
+
       const data = await response.json();
       console.log("Approval response:", data);
-      setFetchedEmails((emails) => emails?.filter((email) => email.id !== emailId));
+      setFetchedEmails((emails) => emails.filter((email) => email.id !== emailId));
       toast("Email approved successfully!", {
-        description: `Email ID}`,
+        description: `Email ID: ${emailId}`,
       });
     } catch (error: any) {
       console.error("Error approving email draft:", error.message);
@@ -72,30 +137,18 @@ export function EmailList({ emails }: EmailListProps) {
     }
   };
 
-
-  const handleEdit = async ({
-    emailId,
-    subject,
-    body,
-  }: {
-    emailId: string;
-    subject: string;
-    body: string;
-  }) => {
+  const handleEdit = async ({ emailId, subject, body }: { emailId: string; subject: string; body: string }) => {
     console.log("Approved");
     toast("Edited the email", {
       description: format(new Date(), "PPpp"),
     });
     console.log(emailId, subject, body);
-    // PUT request
+    // PUT request logic goes here
   };
 
   const handleDelete = async ({ emailId }: { emailId: string }) => {
     try {
-      console.log("Deleting email");
       const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/emails/${emailId}`;
-      console.log("URL: ", url);  // Check if the URL is correct
-
       const response = await fetch(url, {
         method: "DELETE",
         headers: {
@@ -113,7 +166,6 @@ export function EmailList({ emails }: EmailListProps) {
       toast("Deleted the email successfully!", {
         description: format(new Date(), "PPpp"),
       });
-      deleteEmail(emails, emailId);
     } catch (error: any) {
       console.error("Error deleting email:", error.message);
       toast("Failed to delete email", {
@@ -124,13 +176,7 @@ export function EmailList({ emails }: EmailListProps) {
 
   const handleApproveAll = async (userId: string) => {
     try {
-      console.log("Approve All");
-      toast("Pending emails have been scheduled", {
-        description: format(new Date(), "PPpp"),
-      });
-
       const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/schedule/mass`;
-      console.log(url)
       const payload = { userId: userId };
       const response = await fetch(url, {
         method: "POST",
@@ -146,10 +192,7 @@ export function EmailList({ emails }: EmailListProps) {
 
       const data = await response.json();
       console.log("Approval response for all:", data);
-
       setFetchedEmails([]);
-
-
       toast("All emails approved successfully!", {
         description: `Approved emails count: ${data}`,
       });
@@ -161,21 +204,32 @@ export function EmailList({ emails }: EmailListProps) {
     }
   };
 
-  const handleAutopilot = async () => {
-    console.log("Autopilot");
-    toast("Autopilot");
-  };
+  if (loading) {
+    return <div>
+      <Loading></Loading>
+    </div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!fetchedEmails.length) {
+    return (
+      <EmptyState
+        headerMessage="No pending Emails"
+        containerMessage="Go to Scheduled to check currently scheduled emails"
+        icon={<ClockIcon size={80} />}
+      />
+    );
+  }
 
   return (
-    <main className="flex w-full flex-col items-center ">
+    <main className="flex w-full flex-col items-center">
       <div className="mt-6 flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Pending Emails</h1>
 
         <div className="flex flex-row space-x-4">
-          {/* <div className=" flex flex-col space-y-2 ">
-            <Switch id="autopilot-mode" />
-            <Label htmlFor="autopilot-mode">Autopilot</Label>
-          </div> */}
           <div className="flex flex-row m-2">
             <TooltipProvider>
               <Tooltip>
@@ -185,7 +239,7 @@ export function EmailList({ emails }: EmailListProps) {
                       disabled
                       id="autopilot-mode"
                       className="mx-2"
-                      checked={true} // Set to true or false depending on your needs
+                      checked={true}
                     />
                     <Label htmlFor="autopilot-mode" className="my-auto">
                       Autopilot
@@ -198,25 +252,28 @@ export function EmailList({ emails }: EmailListProps) {
               </Tooltip>
             </TooltipProvider>
           </div>
-          {/* <Button onClick={handleAutopilot} className="mx-2">
-            Autopilot
-          </Button> */}
-          <Button onClick={() => { handleApproveAll(getUserId()) }}>
+          <Button onClick={() => handleApproveAll(getUserId())}>
             Approve and Smart Schedule All
           </Button>
         </div>
       </div>
-      <ul className="my-4 w-full space-y-4 ">
+      <ul className="my-4 w-full space-y-4">
         {fetchedEmails.map((item) => (
           <EmailItem
+            key={item.id}
+            item={item}
             handleApprove={handleApprove}
             handleDelete={handleDelete}
             handleEdit={handleEdit}
-            key={item.id}
-            item={item}
           />
         ))}
       </ul>
+      <PaginationWithLinks
+        page={currentPage}
+        pageSize={pageSize}
+        totalCount={fetchedEmails.length}
+        onPageChange={handlePageChange}
+      />
     </main>
   );
 }
