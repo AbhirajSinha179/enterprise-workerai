e783f3574f5fb0c461b8a0cba7d5001cb954ceed
ayanjamil <ayanjamil00@gmail.com>
2024-10-01 22:43:57 +0530

    feat: added tooltip for large emails and centered company cell

--- components/leads/columns.tsx
+++ components/leads/columns.tsx
@@ -76,12 +76,26 @@ export const columns: ColumnDef<Leads>[] = [
             <DataTableColumnHeader
                 column={column}
                 title="Company"
-                className=" flex justify-center w-[100px] "
+                className=" flex justify-center w-[220px] "
                 sortAscending={""}
                 sortDescending={""}
             />
         ),
-        cell: ({ row }) => <div className="flex w-[100px] justify-center text-foreground ">{row.getValue("company") ? row.getValue("company") : "null"}</div>,
+        // cell: ({ row }) => <div className="flex w-[100px] justify-center text-foreground ">{row.getValue("company") ? row.getValue("company") : "null"}</div>,
+        cell: ({ row }) => (
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <div className="flex justify-center text-foreground w-[220px] p-2 rounded-md truncate  ">
+                            {row.getValue("company") ? row.getValue("company") : "null"}
+                        </div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>{row.getValue("company") ? row.getValue("company") : "null"}</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+        ),
         enableSorting: false,
         enableHiding: false,
     },
@@ -91,21 +105,29 @@ export const columns: ColumnDef<Leads>[] = [
             <DataTableColumnHeader
                 column={column}
                 title="Email"
-                className="flex justify-center w-[250px]" // Adjusted width to be consistent
+                className="flex justify-center w-[250px]" // Adjust width as needed
                 sortAscending={""}
                 sortDescending={""}
             />
         ),
         cell: ({ row }) => (
-            <div className="flex justify-center text-foreground overflow-auto">
-                <div className="w-full max-w-[220px] p-2  rounded-md"> {/* Added consistent padding and border-radius */}
-                    {row.getValue("email")}
-                </div>
-            </div>
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <div className="flex justify-start text-foreground w-[220px] p-2 rounded-md truncate ">
+                            {row.getValue("email")}
+                        </div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>{row.getValue("email")}</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
         ),
         enableSorting: false,
         enableHiding: false,
     },
+
 
 
     {
