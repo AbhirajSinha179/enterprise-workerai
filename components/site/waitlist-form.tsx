c8ae658f00a7d04596c8578df79934352c68c9ac
ayanjamil <ayanjamil00@gmail.com>
2024-10-20 21:40:42 +0530

    removed input email fields from landing page and added book a call and tour buttons

--- components/site/waitlist-form.tsx
+++ components/site/waitlist-form.tsx
@@ -1,47 +1,48 @@
 "use client"
-import { useRef, useState } from "react"
-import InputSpotlightBorder from "./input-spotlight-border"
+import { useState } from "react"
 import { Button } from "../ui/button"
+import Link from "next/link"
+import { ClerkLoaded, ClerkLoading, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs"
+import { LoadingSpinner } from "../ui/spinner"
 
 export const WaitlistForm = () => {
-  const [email, setEmail] = useState("")
   const [loading, setLoading] = useState(false)
-  const formRef = useRef<HTMLFormElement>(null)
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    setLoading(true)
-    // Your logic here
-    console.log(email)
-    // fetch here
-
-    setEmail("")
-    // have one case for success and one for error
-    // also add validation on server side, change this to an
-    setLoading(false)
-  }
 
   return (
-    <form
-      ref={formRef}
-      onSubmit={handleSubmit}
-      className="mt-12 flex flex-wrap max-w-3xl relative left-1/2 -translate-x-1/2 items-center justify-center gap-2"
-    >
-      <InputSpotlightBorder
-        type="email"
-        value={email}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e?.target?.value)}
-        placeholder="Enter your email"
-      />
-      <Button
-        variant="secondary"
-        type="submit"
-        className="inline-flex h-12 min-w-fit flex-1 sm:flex-[0.5] animate-shimmer items-center justify-center
+    <div className="mt-12 flex flex-row max-w-3xl relative left-1/2 -translate-x-1/2 items-center justify-center gap-4">
+
+      {/* Book a call button */}
+      <Link href="https://www.calendly.com/rohit_workerai" passHref>
+        <Button
+          variant="secondary"
+          className="inline-flex h-12 w-96 animate-shimmer items-center justify-center
                   rounded-md border bg-[length:200%_100%] px-6 font-medium transition-colors focus:outline-none focus:ring-1 focus:ring-offset-1
-                dark:border-slate-800 dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:text-slate-300 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-50"
-      >
-        {loading ? "Loading..." : "Join the Waitlist"}
-      </Button>
-    </form>
+                  dark:border-slate-800 dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:text-slate-300 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-50"
+        >
+          {loading ? "Loading..." : "Book a call"}
+        </Button>
+      </Link>
+
+      {/* Take a tour button */}
+      <Link href="/tour" passHref>
+        <Button
+          variant="secondary"
+          className="inline-flex h-12 w-96 animate-shimmer items-center justify-center
+                  rounded-md border bg-[length:200%_100%] px-6 font-medium transition-colors focus:outline-none focus:ring-1 focus:ring-offset-1
+                  dark:border-slate-800 dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:text-slate-300 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-50"
+        >
+          <ClerkLoading>
+            <LoadingSpinner />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignInButton>Take a Tour</SignInButton>
+            </SignedOut>
+            <SignedIn>Take a Tour</SignedIn>
+          </ClerkLoaded>
+        </Button>
+      </Link>
+
+    </div>
   )
 }
