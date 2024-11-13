b475d1ebb2703b77072b66a6472dc66dc951af25
AbhirajSinha179 <abhirajsinha179@gmail.com>
2024-11-13 20:07:05 +0530

    fix: force redirect url instead of fallback

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,6 @@ import { SignIn } from "@clerk/nextjs"
 
 export default function Page() {
   return (
-   <SignIn routing="path" path="/sign-in" fallbackRedirectUrl="/dashboard" />
+   <SignIn routing="path" path="/sign-in" forceRedirectUrl="/dashboard" />
   )
 }
