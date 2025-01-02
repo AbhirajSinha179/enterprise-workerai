efbb2e0c49b55d27e3d9b4779f3d09eee76ae149
ayanjamil <ayanjamil00@gmail.com>
2025-01-03 02:28:57 +0530

    fix : mailbox fix

--- app/(main)/dashboard/(mailbox)/mailbox/page.tsx
+++ app/(main)/dashboard/(mailbox)/mailbox/page.tsx
@@ -1,13 +1,9 @@
-"use client"
-// import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server"
 import Link from "next/link"
 import { ContentLayout } from "@/components/layout/content-layout"
 import { DataTable } from "@/components/leads/data-table"
 import { columns } from "@/components/mailbox/columns"
 import { Button } from "@/components/ui/button"
-import { useTargetContext } from "@/contexts/TargetIdContext";
-import { useEffect, useState } from "react"
-
 
 // {
 //   id: 'b2305391-8bae-408c-8d7f-57f115a50c91',
@@ -20,8 +16,10 @@ import { useEffect, useState } from "react"
 //   dailyCapacity: 25
 // }
 
-const getMails = async (id: string, type:"u"|"t") => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user/email-address/${id}?t=${type}`)
+const getMails = async (userId: string) => {
+  const res = await fetch(`${process.env.BASE_API_URL}/user/email-address/${userId}`, {
+    next: { revalidate: 30 },
+  })
   if (!res.ok) {
     return null
   }
@@ -31,52 +29,29 @@ const getMails = async (id: string, type:"u"|"t") => {
 }
 
 export default async function MailboxPage() {
-  const { targetId } = useTargetContext();
-  // console.log("TARGET ID FORM THE CONTEXT : ", targetId)
-
-  // const { userId } = auth()
-  // if (!userId) {
-  //   return {
-  //     redirect: {
-  //       destination: "/",
-  //       permanent: false,
-  //     },
-  //   }
-  // }
-  const [mailsDisplay,setMailsDisplay]= useState<any[]>([])
-
-  useEffect(() => {
-    if (!targetId) {
-      console.warn("No target ID available.");
-      return;
-    }
-    const fetchMails = async () => {
-      const fetchedMails = await getMails(targetId, "t");
-      setMailsDisplay(fetchedMails)
-    };
-    fetchMails();
-  }, [targetId])
-  
-  
-  if (targetId) {
-    if (!mailsDisplay) {
-      return (
-        <ContentLayout title="Mailbox">
-          <div className="flex min-h-[70vh] items-center justify-center">
-            <div className="flex items-center justify-center font-bold">
-              Unable to fetch data. Please try again later.
-            </div>
-          </div>
-        </ContentLayout>
-      )
+  const { userId } = auth()
+  if (!userId) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
     }
   }
-  else
-    console.log("TARGET ID NOT FOUND ")
-
+  const mails = await getMails(userId)
   // console.log("MAILS INFO :", mails)
 
-
+  if (!mails) {
+    return (
+      <ContentLayout title="Mailbox">
+        <div className="flex min-h-[70vh] items-center justify-center">
+          <div className="flex items-center justify-center font-bold">
+            Unable to fetch data. Please try again later.
+          </div>
+        </div>
+      </ContentLayout>
+    )
+  }
   return (
     <ContentLayout title="Mailbox">
       <div className="my-8 flex justify-between">
@@ -88,7 +63,7 @@ export default async function MailboxPage() {
         </Link>
       </div>
       {/* @ts-ignore */}
-      <DataTable data={mailsDisplay} columns={columns} isActionButton={false} />
+      <DataTable data={mails} columns={columns} isActionButton={false} />
     </ContentLayout>
   )
 }
