6eca69d212e1be7128d6b0c8c281445ef3067e40
AbhirajSinha179 <abhirajsinha179@gmail.com>
2025-01-30 21:50:01 +0530

    fix: overview redirect on sign in

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,6 @@ import { SignIn } from "@clerk/nextjs"
 
 export default function Page() {
   return (
-   <SignIn routing="path" path="/sign-in" forceRedirectUrl="/dashboard" />
+   <SignIn routing="path" path="/sign-in" forceRedirectUrl="/overview" />
   )
 }
