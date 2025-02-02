42ad8e5bffa656d44b278ff1d4d73f63d8225234
ayanjamil <ayanjamil00@gmail.com>
2025-02-02 22:50:40 +0530

    fix (mailbos) : laoding skeleton and paggination component fix

--- components/leads/data-table-pagination.tsx
+++ components/leads/data-table-pagination.tsx
@@ -34,7 +34,7 @@ export function DataTablePagination<TData>({
   return (
     <div className="flex items-center justify-between px-2">
       <div className="flex-1 text-sm text-foreground">
-        {selectedRowCount} of {filteredRowCount} row(s) selected.
+        {/* {selectedRowCount} of {filteredRowCount} row(s) selected. */}
       </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
