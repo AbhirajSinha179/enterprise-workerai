059696976cc3638e8e12214800b246904103e80c
ayanjamil <ayanjamil00@gmail.com>
2025-03-06 01:17:15 +0530

    feat: switch to shadcn chats

--- components/dashboard/overview.tsx
+++ components/dashboard/overview.tsx
@@ -3,13 +3,21 @@
 import {
   AreaChart,
   Area,
-  CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
 import { Skeleton } from "@/components/ui/skeleton";
+import { TrendingUp } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 interface DataItem {
   date: string | null;
@@ -19,17 +27,15 @@ interface DataItem {
 
 interface OverviewProps {
   data: DataItem[];
-  isLoading?: boolean; // New prop to indicate loading state
+  isLoading?: boolean;
 }
 
 export function Overview({ data, isLoading }: OverviewProps) {
   if (isLoading) {
-    // Render a skeleton while loading
     return (
       <div className="w-full h-[200px] sm:h-[300px] md:h-[350px]">
         <Skeleton className="h-full w-full" />
       </div>
-
     );
   }
 
@@ -44,17 +50,15 @@ export function Overview({ data, isLoading }: OverviewProps) {
   return (
     <ResponsiveContainer width="100%" height={350}>
       <AreaChart
-        width={730}
-        height={300}
         data={data}
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
       >
         <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <linearGradient id="colorTotalEmails" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
             <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
           </linearGradient>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+          <linearGradient id="colorOpens" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#435BD5" stopOpacity={1.0} />
             <stop offset="95%" stopColor="#435BD5" stopOpacity={0} />
           </linearGradient>
@@ -64,6 +68,7 @@ export function Overview({ data, isLoading }: OverviewProps) {
           tickLine={false}
           axisLine={false}
           tickMargin={8}
+          interval={Math.ceil(data.length / 6)}
           tickFormatter={(value) => value.slice(0, 8)}
         />
         <YAxis
@@ -71,28 +76,38 @@ export function Overview({ data, isLoading }: OverviewProps) {
           tickFormatter={(value) => (value === 0 ? "" : value)}
         />
         <Tooltip
-          formatter={(value, name) => {
+          formatter={(value, name, props) => {
             const customNames: { [key: string]: string } = {
               total_emails: "Total Emails",
               opens: "Opens",
             };
             return [value, customNames[name] || name];
           }}
+          labelFormatter={(label) => `Date: ${label}`}
+          content={({ label, payload }) => (
+            <div className="p-2 bg-white shadow rounded text-black">
+              <p className="font-bold">Date: {label}</p>
+              {payload?.map((entry, index) => (
+                <p key={index} style={{ color: entry.color }}>
+                  {entry.name}: {entry.value}
+                </p>
+              ))}
+            </div>
+          )}
         />
         <Area
           type="monotone"
           dataKey="total_emails"
           stroke="#8884d8"
           fillOpacity={1}
-          fill="url(#colorUv)"
+          fill="url(#colorTotalEmails)"
         />
         <Area
           type="monotone"
           dataKey="opens"
-          label="Opens"
           stroke="#435BD5"
           fillOpacity={1}
-          fill="url(#colorPv)"
+          fill="url(#colorOpens)"
         />
       </AreaChart>
     </ResponsiveContainer>
