490679f3c326490d206a340aeeab894e1ff7c8a4
ayanjamil <ayanjamil00@gmail.com>
2025-03-06 22:42:21 +0530

    fix : shifting to shadcn charts

--- components/dashboard/overview.tsx
+++ components/dashboard/overview.tsx
@@ -1,24 +1,13 @@
 "use client";
 
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
 import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 import { Skeleton } from "@/components/ui/skeleton";
-import { TrendingUp } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
 interface DataItem {
   date: string | null;
   opens: number | null;
@@ -29,6 +18,17 @@ interface OverviewProps {
   data: DataItem[];
   isLoading?: boolean;
 }
+
+const chartConfig = {
+  total_emails: {
+    label: "Total Emails",
+    color: "#8884d8",
+  },
+  opens: {
+    label: "Opens",
+    color: "#435BD5",
+  },
+} satisfies ChartConfig;
 
 export function Overview({ data, isLoading }: OverviewProps) {
   if (isLoading) {
@@ -47,69 +47,56 @@ export function Overview({ data, isLoading }: OverviewProps) {
     );
   }
 
+  const sanitizedData = data.map((item) => ({
+    ...item,
+    total_emails: Math.max(0, item.total_emails || 0),
+    opens: Math.max(0, item.opens || 0),
+  }));
+
   return (
-    <ResponsiveContainer width="100%" height={350}>
-      <AreaChart
-        data={data}
-        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-      >
-        <defs>
-          <linearGradient id="colorTotalEmails" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-          </linearGradient>
-          <linearGradient id="colorOpens" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#435BD5" stopOpacity={1.0} />
-            <stop offset="95%" stopColor="#435BD5" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <XAxis
-          dataKey="date"
-          tickLine={false}
-          axisLine={false}
-          tickMargin={8}
-          interval={Math.ceil(data.length / 6)}
-          tickFormatter={(value) => value.slice(0, 8)}
-        />
-        <YAxis
-          axisLine={false}
-          tickFormatter={(value) => (value === 0 ? "" : value)}
-        />
-        <Tooltip
-          formatter={(value, name, props) => {
-            const customNames: { [key: string]: string } = {
-              total_emails: "Total Emails",
-              opens: "Opens",
-            };
-            return [value, customNames[name] || name];
-          }}
-          labelFormatter={(label) => `Date: ${label}`}
-          content={({ label, payload }) => (
-            <div className="p-2 bg-white shadow rounded text-black">
-              <p className="font-bold">Date: {label}</p>
-              {payload?.map((entry, index) => (
-                <p key={index} style={{ color: entry.color }}>
-                  {entry.name}: {entry.value}
-                </p>
-              ))}
-            </div>
-          )}
-        />
-        <Area
-          type="monotone"
-          dataKey="total_emails"
-          stroke="#8884d8"
-          fillOpacity={1}
-          fill="url(#colorTotalEmails)"
-        />
-        <Area
-          type="monotone"
-          dataKey="opens"
-          stroke="#435BD5"
-          fillOpacity={1}
-          fill="url(#colorOpens)"
-        />
-      </AreaChart>
-    </ResponsiveContainer>
+    <div className="max-h-[350px] overflow-hidden w-full flex justify-center">
+      <ChartContainer config={chartConfig} className="max-h-[350px] w-full">
+        <AreaChart
+          accessibilityLayer
+          data={sanitizedData}
+          height={350}
+          margin={{ left: 12, right: 12 }}
+        >
+          <CartesianGrid vertical={false} horizontal={false} />
+          <XAxis
+            dataKey="date"
+            tickLine={false}
+            axisLine={false}
+            tickMargin={8}
+            tickFormatter={(value) => value.slice(0, 8)}
+          />
+          <YAxis
+            axisLine={false}
+            tickLine={false}
+            tickFormatter={(value) => value}
+            width={40}
+          />
+
+          <ChartTooltip
+            cursor={false}
+            content={<ChartTooltipContent indicator="line" />}
+          />
+          <Area
+            dataKey="total_emails"
+            type="monotone"
+            fill="#8884d8"
+            fillOpacity={0.4}
+            stroke="#8884d8"
+          />
+          <Area
+            dataKey="opens"
+            type="monotone"
+            fill="#435BD5"
+            fillOpacity={0.4}
+            stroke="#435BD5"
+          />
+        </AreaChart>
+      </ChartContainer>
+    </div>
   );
 }
